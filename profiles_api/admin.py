77ca6f61d36d2de855e03a167bc4d51ee89fc158
Marcin <marcin.karbowniczyn@gmail.com>
2024-01-29 17:39:04 +0100

    Enable Django admin for User Profile model

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# By default, the Django Admin is enabled ion all new project,
+# however we need to register any newly created models with the Django admin, so it knows that I want to display that model in the admin UI.
+
+admin.site.register(models.UserProfile)
